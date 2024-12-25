13e28e8bdd4d08bedb2054f1fda45884ad734177
akhilrale11 <117888290+akhilrale11@users.noreply.github.com>
2024-12-25 19:46:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+  code.addEventListener('input', (e) => {
+    const nextInput = codes[idx + 1];
+    if (e.target.value && nextInput) {
+      nextInput.focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (!e.target.value && idx > 0) {
+        codes[idx - 1].focus();
+      }
+    }
+  });
+});
